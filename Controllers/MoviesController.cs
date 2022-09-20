3b553afd1bc3144ac418e13476edcc2747d8aad0
Khawlabenmansour13 <khawla.benmansour@esprit.tn>
2022-09-20 16:25:32 +0100

    CreateMovie

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using DotNetCore6Crud.Models;
+using DotNetCore6Crud.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCore6Crud.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task <IActionResult> Index()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return View(movies);
+        }
+        public async Task<IActionResult> Create()
+        {
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync()
+            };
+
+            return View  (viewModel);
+        }
+    }
+}
